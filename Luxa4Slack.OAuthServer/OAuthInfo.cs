28880b9168cd3bee36e254f8d94e3733829c84c5
Gregoire Pailler <gregoire@pailler.fr>
2016-06-23 22:01:37 +0800

    Added Luxa4Slack.OAuthServer project

---
+++ Luxa4Slack.OAuthServer/OAuthInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace CG.Luxa4Slack
+{
+  using System;
+
+  public static class OAuthInfo
+  {
+    public const string RedirectedUri = "https://luxa4slack.awan.tech/OAuth";
+
+    public const string ClientId = "3120088010.52664063651";
+
+    public static readonly string SecretId = Environment.GetEnvironmentVariable("LUXA4SLACK_SECRETID");
+
+    public static readonly Uri SlackOAuthUri = new Uri("https://slack.com/api/oauth.access");
+  }
+}
---
+++ Luxa4Slack.OAuthServer/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace CG.Luxa4Slack.OAuthServer
+{
+  using Microsoft.AspNetCore.Builder;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Logging;
+
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.UseExceptionHandler("/Error");
+
+      app.UseMvc(
+        routes =>
+          {
+            routes.MapRoute(
+              name: "Default",
+              template: "{action}",
+              defaults: new { controller = "Default", action = "Index" });
+          });
+    }
+  }
+}

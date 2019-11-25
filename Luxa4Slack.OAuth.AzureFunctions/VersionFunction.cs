3d205fbf6fa3ae2d58739dbeb1c157d53395aa16
Gregoire Pailler <gregoire@pailler.fr>
2019-11-25 22:45:54 +0800

    Replaced OAuth webapp with Azure functions

---
+++ Luxa4Slack.OAuth.AzureFunctions/VersionFunction.cs
@@ -0,0 +1,24 @@
+﻿namespace Luxa4Slack.OAuth.AzureFunctions
+{
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.Azure.WebJobs;
+  using Microsoft.Azure.WebJobs.Extensions.Http;
+  using Microsoft.AspNetCore.Http;
+  using Microsoft.Extensions.Logging;
+
+  public class VersionFunction
+  {
+    [FunctionName("Version")]
+    public IActionResult Run(
+      [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+      HttpRequest request,
+      ILogger log)
+    {
+      log.LogInformation("'Version' HTTP trigger function processed a request.");
+
+      var assembly = this.GetType().Assembly.GetName();
+      return new OkObjectResult($"{assembly.Name} - {assembly.Version}");
+    }
+  }
+}
+

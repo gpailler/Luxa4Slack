33d4f2c82d88ad0b4f6b784985f443f1207913f6
Gregoire Pailler <gregoire@pailler.fr>
2021-06-10 11:50:07 +0800

    Update build script to latest version and to build self contained version of Luxa4Slack

--- build/Build.cs
+++ build/Build.cs
@@ -1,24 +1,20 @@
-using System;
 using System.Linq;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.MSBuild;
-using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.IO.CompressionTasks;
 using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Nuke.Common.IO.TextTasks;
-using static Nuke.Common.Logger;
-using static Nuke.Common.Tools.GitVersion.GitVersionTasks;
-using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
-using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using static Nuke.Common.IO.CompressionTasks;
 
 [CheckBuildProjectConfigurations]
-[UnsetVisualStudioEnvironmentVariables]
+[ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Compile);
@@ -26,17 +22,14 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution]
-    readonly Solution Solution;
-
-    [GitVersion(Framework = "netcoreapp2.1")]
-    readonly GitVersion GitVersion;
+    [Solution] readonly Solution Solution;
+    [GitVersion(Framework = "net5.0", NoFetch = true)] readonly GitVersion GitVersion;
 
     [PackageExecutable(
-        packageId: "NSIS-Tool",
-        packageExecutable: "makensis.exe",
-        Version = "3.0.5",
-        Framework = "tools")]
+      packageId: "NSIS-Tool",
+      packageExecutable: "makensis.exe",
+      Version = "3.0.6.1",
+      Framework = "tools")]
     readonly Tool MakeNsis;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
@@ -51,7 +44,9 @@ class Build : NukeBuild
 
     AbsolutePath InstallerVersionsFile => SourceDirectory / "Luxa4Slack.Installer" / "Versions.nsh";
 
-    AbsolutePath GlobalAssemblyInfoFile => SourceDirectory / "GlobalAssemblyInfo.cs";
+    AbsolutePath PublishedProjectDirectory => SourceDirectory / "Luxa4Slack.Tray";
+
+    const string PublishPlatform = "win-x64";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -66,63 +61,66 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
-            NuGetRestore(_ => _
-                .SetTargetPath(Solution)
-            );
-        });
-
-    Target PatchVersion => _ => _
-        .Executes(() =>
-        {
-            Info("Patch " + GlobalAssemblyInfoFile);
-            GitVersion(_ => _
-                .SetFramework("netcoreapp2.1")
-                .SetArgumentConfigurator(_ => _
-                    .Add("/updateassemblyinfo {value}", GlobalAssemblyInfoFile)
-                )
-            );
-
-            Info("Patch " + InstallerVersionsFile);
-            WriteAllLines(InstallerVersionsFile,
-                new[]
-                {
-                    $"!define VERSIONMAJOR {GitVersion.Major}",
-                    $"!define VERSIONMINOR {GitVersion.Minor}",
-                    $"!define VERSIONPATCH {GitVersion.Patch}"
-                });
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore, PatchVersion)
-        .Executes(() =>
-        {
-            MSBuild(_ => _
-                .SetTargetPath(Solution)
-                .SetTargets("Rebuild")
-                .SetConfiguration(Configuration)
-                .SetMaxCpuCount(Environment.ProcessorCount)
-                .SetNodeReuse(IsLocalBuild));
-        });
+      .DependsOn(Restore)
+      .Executes(() =>
+      {
+        DotNetBuild(s => s
+          .SetProjectFile(Solution)
+          .SetConfiguration(Configuration)
+          .SetAssemblyVersion(GitVersion.AssemblySemVer)
+          .SetFileVersion(GitVersion.AssemblySemFileVer)
+          .SetInformationalVersion(GitVersion.InformationalVersion)
+          .SetNoRestore(true));
+      });
+
+    Target Publish => _ => _
+      .DependsOn(Clean)
+      .Executes(() =>
+      {
+        DotNetPublish(s => s
+          .SetProject(PublishedProjectDirectory)
+          .SetConfiguration(Configuration)
+          .SetAssemblyVersion(GitVersion.AssemblySemVer)
+          .SetFileVersion(GitVersion.AssemblySemFileVer)
+          .SetInformationalVersion(GitVersion.InformationalVersion)
+          .SetRuntime(PublishPlatform)
+          .SetOutput(OutputDirectory)
+          .SetSelfContained(true)
+          .SetPublishSingleFile(false));
+      });
 
     Target Pack => _ => _
-        .DependsOn(Clean, Compile)
-        .Executes(() =>
-        {
-            var extensions = new[] { ".exe", ".exe.config", ".dll" };
+      .DependsOn(Clean, Publish)
+      .Executes(() =>
+      {
+        var extensions = new[] { ".exe", ".exe.config", ".dll", "*.pdb", "*.json", "*.config" };
 
-            CompressZip(
-                OutputDirectory,
-                ArtifactFile,
-                info => extensions.Any(extension => info.Name.EndsWith(extension)));
-        });
+        CompressZip(
+          OutputDirectory,
+          ArtifactFile,
+          info => extensions.Any(extension => info.Name.EndsWith(extension)));
+      });
 
     Target BuildInstaller => _ => _
-        .DependsOn(Clean, Compile)
-        .Executes(() =>
-        {
-            MakeNsis(
-                arguments: $"/V4 /DCONFIGURATION={Configuration} {InstallerFile}",
-                workingDirectory: InstallerFile.Parent,
-                logOutput: true);
-        });
+      .DependsOn(Clean, Publish)
+      .Executes(() =>
+      {
+        WriteAllLines(InstallerVersionsFile,
+          new[]
+          {
+            $"!define VERSIONMAJOR {GitVersion.Major}",
+            $"!define VERSIONMINOR {GitVersion.Minor}",
+            $"!define VERSIONPATCH {GitVersion.Patch}"
+          });
+
+        MakeNsis(
+          arguments: $"/V4 /DCONFIGURATION={Configuration} {InstallerFile}",
+          workingDirectory: InstallerFile.Parent,
+          logOutput: true);
+      });
 }

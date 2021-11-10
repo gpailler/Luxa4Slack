73540ef73cf948d83279081202cc6bf784f8618d
Gregoire Pailler <gregoire@pailler.fr>
2021-11-10 09:38:03 +0800

    Use .NET 6 builtin format command

--- format.ps1
+++ format.ps1
@@ -1,4 +1,1 @@
-# Install dotnet-format if needed
-# dotnet tool install -g dotnet-format --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json
-
-dotnet format --fix-whitespace --fix-style info Luxa4Slack.sln --exclude vendor\**\* -v diag
+dotnet format style --exclude vendor\**\* --verbosity diagnostic

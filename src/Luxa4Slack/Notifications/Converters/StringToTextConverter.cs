2472f7a1e600a172fcad3d6554b9a6dfbdafd3b1
Grégoire Pailler <gregoire@pailler.fr>
2021-11-10 09:44:08 +0800

    Workaround to properly deserialize blocks messages containing only a string instead of a Text object (#26)

---
+++ src/Luxa4Slack/Notifications/Converters/StringToTextConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace CG.Luxa4Slack.Notifications.Converters
+{
+  using System;
+  using Newtonsoft.Json;
+  using SlackAPI;
+
+  internal class StringToTextConverter : JsonConverter
+  {
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+      var instance = new Text();
+      if (reader.TokenType == JsonToken.String)
+      {
+        // Handle blocks containing only a string instead of a Text object (Giphy messages for example)
+        instance.text = reader.Value as string;
+      }
+      else
+      {
+        serializer.Populate(reader, instance);
+      }
+
+      return instance;
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+      return objectType == typeof(Text);
+    }
+  }
+}

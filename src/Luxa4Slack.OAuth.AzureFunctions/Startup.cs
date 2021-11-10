af3b89864f577d7fdc36272688779f547b0e6c03
Gregoire Pailler <gregoire@pailler.fr>
2021-11-10 10:53:31 +0800

    Update azure functions to .NET 6.0 + v4. Add Startup to inject IHttpClientFactory

---
+++ src/Luxa4Slack.OAuth.AzureFunctions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+[assembly: FunctionsStartup(typeof(Luxa4Slack.OAuth.AzureFunctions.Startup))]
+
+namespace Luxa4Slack.OAuth.AzureFunctions
+{
+  using Microsoft.Extensions.DependencyInjection;
+
+  public class Startup : FunctionsStartup
+  {
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+      builder.Services.AddHttpClient();
+    }
+  }
+}
